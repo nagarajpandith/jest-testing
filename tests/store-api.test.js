2b8439045adbbd3cc7b23fce0b1f74940d670955
NidheeshaT <ntbayari@gmail.com>
2023-10-29 20:27:43 +0530

    added fake api 'server

--- tests/store-api.test.js
+++ tests/store-api.test.js
@@ -14,12 +14,14 @@ test('Make sure the route /chocolates is working',async  () => {
     const res= await  fetch('http://localhost:5000/chocolates')
     expect(res.status).toBe(200)
     const json= await res.json()
+    expect(json).not.toBeNull()
     expect(json).toEqual(chocolatesList)
 });
 
 test('Make sure the route /chocolat not is working',async  () => {
     const res= await  fetch('http://localhost:5000/chocolat')
     expect(res.status).toBe(404)
+    expect(await res.json()).toEqual({})
 });
 
 });
